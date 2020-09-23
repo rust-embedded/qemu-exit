657982bb0e8bec6c9f9624bc473a524d091d873e
Esteban Blanc <estblcsk@gmail.com>
2020-09-23 08:49:59 +0200

    x86: Add exit_success and exit_failure

--- src/x86.rs
+++ src/x86.rs
@@ -6,17 +6,23 @@
 
 use crate::QemuExit;
 
+/// QEMU exit code 1
+pub const EXIT_FAILURE: u32 = 0; // as ((0 << 1) | 1) = 1
+
 /// X86 QemuExit info struct
 pub struct X86 {
     /// Port number of the isa-debug-exit device
     io_port: u16,
+    /// As QEMU cannot exit(0), chose a value that represent succes code. It must be ood
+    exit_success: u32,
 }
 
 impl X86 {
     /// Create a new X86 QemuExit struct
-    pub fn new<T: Into<u16>>(io_port: T) -> Self {
+    pub fn new<T: Into<u16>, U: Into<u32>>(io_port: T, exit_success: U) -> Self {
         X86 {
             io_port: io_port.into(),
+            exit_success: exit_success.into(),
         }
     }
 }
@@ -34,5 +40,14 @@ impl QemuExit for X86 {
         // the last expression in the `panic!()` handler itself. This prevents a possible
         // infinite loop.
         loop {}
+    }
+
+    fn exit_success(&self) -> ! {
+        let code = self.exit_success >> 1; // Shift because QEMU does ((code << 1) | 1)
+        self.exit(code)
+    }
+
+    fn exit_failure(&self) -> ! {
+        self.exit(EXIT_FAILURE)
     }
 }
