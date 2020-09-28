36a1586fda728da5a5de060c657be26cc1e5dc95
Esteban Blanc <estblcsk@gmail.com>
2020-09-28 21:59:45 +0200

    tests: Add riscv64 opensbi bios

--- tests/test_runner_wrapper.sh
+++ tests/test_runner_wrapper.sh
@@ -6,7 +6,7 @@ if [[ $1 == *"aarch64"* ]]; then
 
     qemu-system-aarch64 -M raspi3 -display none -semihosting -kernel $STRIPPED_BINARY
 elif [[ $1 == *"riscv64"* ]]; then
-    qemu-system-riscv64 -M virt -bios default -display none -kernel $1
+    qemu-system-riscv64 -M virt -bios tests/riscv64_virt/fw_jump.elf -display none -kernel $1
 fi
 
 let "status = $? - 13"
