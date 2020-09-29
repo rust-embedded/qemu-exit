12f375e2405023d48b4a29bbcf433b3090f6f4c0
Andre Richter <andre.o.richter@gmail.com>
2020-09-29 21:20:24 +0200

    Guard qemu run with a timeout

--- tests/test_runner_wrapper.sh
+++ tests/test_runner_wrapper.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+TIMEOUT="timeout 10"
+
 if [[ $1 == *"aarch64"* ]]; then
     rust-objcopy --strip-all -O binary $1 $1.img
     STRIPPED_BINARY=$(echo $1.img | sed -e 's/.*target/target/g')
 
-    qemu-system-aarch64 -M raspi3 -display none -semihosting -kernel $STRIPPED_BINARY
+    $TIMEOUT qemu-system-aarch64 -M raspi3 -display none -semihosting -kernel $STRIPPED_BINARY
 elif [[ $1 == *"riscv64"* ]]; then
-    qemu-system-riscv64 -M virt -bios tests/riscv64_virt/fw_jump.elf -display none -kernel $1
+    $TIMEOUT qemu-system-riscv64 -M virt -bios tests/riscv64_virt/fw_jump.elf -display none -kernel $1
 fi
 
 let "status = $? - 13"
