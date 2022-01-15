c0bd96fb7f824e6e535458d55d16afd8a66aa365
Andre Richter <andre.o.richter@gmail.com>
2022-01-15 22:01:28 +0100

    Update copyright year

--- tests/riscv64_virt/mod.rs
+++ tests/riscv64_virt/mod.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 //
-// Copyright (c) 2019-2021 Esteban Blanc <estblcsk@gmail.com>
+// Copyright (c) 2019-2022 Esteban Blanc <estblcsk@gmail.com>
 
 //! RISCV64 specific setup code.
 
