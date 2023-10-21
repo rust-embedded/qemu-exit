05c60dc10b1b7dd56911c77421eec2c84fcdb38c
Alex Martens <alex@thinglab.org>
2023-10-21 09:57:02 +0900

    Fix CI

--- tests/test_runner_wrapper.sh
+++ tests/test_runner_wrapper.sh
@@ -3,7 +3,7 @@
 TIMEOUT="timeout 10"
 
 if [[ $1 == *"aarch64"* ]]; then
-    $TIMEOUT qemu-system-aarch64 -M raspi3 -display none -semihosting -kernel $1
+    $TIMEOUT qemu-system-aarch64 -M raspi3b -display none -semihosting -kernel $1
 elif [[ $1 == *"riscv64"* ]]; then
     $TIMEOUT qemu-system-riscv64 -M virt -bios tests/riscv64_virt/fw_jump.elf -display none -kernel $1
 fi
